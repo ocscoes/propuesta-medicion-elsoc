4c51997a8ad2d8bf4a3313139dc1d406b300f2fd
Tomás Urzúa Moreno <tomas.urzua@ug.uchile.cl>
2025-08-21 12:45:57 -0400

    up cruces

---
+++ processing/proc-migracion.r
@@ -0,0 +1,63 @@
+pacman::p_load(dplyr, car, summarytools, sjmisc, sjlabelled)
+
+load('input/data/raw_data/elsoc.RData')
+
+data <- elsoc_long_2016_2023
+rm(elsoc_long_2016_2023)
+
+# seleccionar solo ola 2022
+data <- data %>%
+  filter(ola==6)
+
+#seleccionar variables de interes
+data <- data %>%
+  select(idencuesta, m0_sexo, m0_edad, comuna, comuna_cod, region, region_cod, 
+         t06_01, t10, t09_01, t09_02, t09_03, t02_01, t02_02,
+         t02_03, t02_04, t03_01, t03_02, t03_03, t03_04, r07, r09,
+          r12_03, r12_04, r16, r18_01, r18_02)
+
+#cambiar nombre a variables
+data <- data %>% 
+  rename(sexo = m0_sexo, edad = m0_edad, seguridad_sat = t06_01, 
+         seguridad_perc = t10, peleas_calle = t09_01,
+         asaltos = t09_02, trafico_drogas = t09_03,
+         barrio_ideal = t02_01,
+         barrio_integracion = t02_02, barrio_identidad = t02_03, 
+         barrio_pertenencia = t02_04, barrio_amigos = t03_01, 
+         barrio_sociable = t03_02, barrio_cordial = t03_03, 
+         barrio_colaborador = t03_04,
+         contacto_migrantes = r07, simpatia_migrantes = r09,
+         perdida_identidad = r12_03, desempleo_migrantes = r12_04,
+         confianza_migrantes = r16, fomentar_migracion = r18_01,
+         igualdad_migrantes = r18_02)
+
+#Recodificar NA
+data$seguridad_sat <- recode(data$seguridad_sat, "c(-666, -777, -888, -999) = NA") 
+data$seguridad_perc <- recode(data$seguridad_perc, "c(-666, -777, -888, -999) = NA")
+data$peleas_calle <- recode(data$peleas_calle, "c(-666, -777, -888, -999) = NA")
+data$asaltos <- recode(data$asaltos, "c(-666, -777, -888, -999) = NA")
+data$trafico_drogas <- recode(data$trafico_drogas, "c(-666, -777, -888, -999) = NA")
+data$barrio_ideal <- recode(data$barrio_ideal, "c(-666, -777, -888, -999) = NA")
+data$barrio_integracion <- recode(data$barrio_integracion, "c(-666, -777, -888, -999) = NA")
+data$barrio_identidad <- recode(data$barrio_identidad, "c(-666, -777, -888, -999) = NA")
+data$barrio_pertenencia <- recode(data$barrio_pertenencia, "c(-666, -777, -888, -999) = NA")
+data$barrio_amigos <- recode(data$barrio_amigos, "c(-666, -777, -888, -999) = NA")
+data$barrio_sociable <- recode(data$barrio_sociable, "c(-666, -777, -888, -999) = NA")
+data$barrio_cordial <- recode(data$barrio_cordial, "c(-666, -777, -888, -999) = NA")
+data$barrio_colaborador <- recode(data$barrio_colaborador, "c(-666, -777, -888, -999) = NA")
+data$contacto_migrantes <- recode(data$contacto_migrante, "c(-666, -777, -888, -999) = NA")
+data$simpatia_migrantes <- recode(data$simpatia_migrantes, "c(-666, -777, -888, -999) = NA")
+data$perdida_identidad <- recode(data$perdida_identidad, "c(-666, -777, -888, -999) = NA")
+data$desempleo_migrantes <- recode(data$desempleo_migrantes, "c(-666, -777, -888, -999) = NA")
+data$confianza_migrantes <- recode(data$confianza_migrantes, "c(-666, -777, -888, -999) = NA")
+data$fomentar_migracion <- recode(data$fomentar_migracion, "c(-666, -777, -888, -999) = NA")
+data$igualdad_migrantes <- recode(data$igualdad_migrantes, "c(-666, -777, -888, -999) = NA")
+data$sexo <- recode(data$sexo, "c(1) = 0; c(2) = 1")
+
+# invertir escala de seguridad objetiva para que esté alineada con seguridad subjetiva
+data$peleas_calle <- car::recode(data$peleas_calle, "(1)=5; (2)=4; (3)=3; (4)=2; (5)=1")
+data$asaltos <- car::recode(data$asaltos, "(1)=5; (2)=4; (3)=3; (4)=2; (5)=1")
+data$trafico_drogas <- car::recode(data$trafico_drogas, "(1)=5; (2)=4; (3)=3; (4)=2; (5)=1")
+
+data_mig <- data
+saveRDS(data_mig, file="input/data/proc_data/elsoc_2022_mig.RData")
